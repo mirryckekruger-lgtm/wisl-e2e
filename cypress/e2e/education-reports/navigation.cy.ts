1a7dfe0ca48183988f59b412027f980412f7ca77
RKruyshaar <ruan.kruyshaar@potch.matogen.com>
2025-11-06 12:51:59 +0200

    Add contexts for different offices

--- cypress/e2e/education-reports/navigation.cy.ts
+++ cypress/e2e/education-reports/navigation.cy.ts
@@ -1,17 +1,41 @@
 describe('Navigating Education Side Bar Menu', () => {
-  beforeEach(() => {
-    // Arrange
-    cy.login();
-    cy.visit('https://test.wisl.nwu.ac.za/nav/systemAdministrationOverview/');
-    cy.url().should('include', '/systemAdministrationOverview');
+  context('with education office', () => {
+    beforeEach(() => {
+      // Arrange
+      cy.login();
+      cy.visit('https://test.wisl.nwu.ac.za/nav/systemAdministrationOverview/');
+      cy.url().should('include', '/systemAdministrationOverview');
+      // Education is selected by default
+    });
+
+    it('navigates to Education Reports', () => {
+      cy.get('button:contains("Reports")').eq(1).click();
+      cy.get('button')
+        .contains(/^ Placements$/)
+        .closest('button')
+        .click();
+      cy.get('button').contains('Total Placements').closest('button').click();
+      cy.url().should('include', 'reportOnCampusPlacements/main');
+    });
   });
 
-  it('navigates to Education Reports', () => {
-    cy.get('button:contains("Reports")').eq(1).click();
-    cy.pause()
-    cy.get('button').contains('Placements').closest('button').click();
-     cy.get('button').contains('Total Placements').closest('button').click();
-    cy.url().should('include', 'reportOnCampusPlacements/main');
-  })
+  context('with nursing office', () => {
+    beforeEach(() => {
+      // Arrange
+      cy.login();
+      cy.visit('https://test.wisl.nwu.ac.za/nav/systemAdministrationOverview/');
+      cy.url().should('include', '/systemAdministrationOverview');
+      // Select the office
+    });
 
-})
+    it('navigates to Education Reports', () => {
+      cy.get('button:contains("Reports")').eq(1).click();
+      cy.get('button')
+        .contains(/^ Placements$/)
+        .closest('button')
+        .click();
+      cy.get('button').contains('Total Placements').closest('button').click();
+      cy.url().should('include', 'reportOnCampusPlacements/main');
+    });
+  });
+});
