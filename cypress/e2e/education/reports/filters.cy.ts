67f0ca6c5d2c635235242e90e5aeb3741061ec64
RKruyshaar <ruan.kruyshaar@potch.matogen.com>
2025-11-14 10:02:38 +0200

    Test filtering by year level

--- cypress/e2e/education/reports/filters.cy.ts
+++ cypress/e2e/education/reports/filters.cy.ts
@@ -5,16 +5,27 @@ describe('Navigating Education Side Bar Menu', () => {
     cy.visit('https://test.wisl.nwu.ac.za/nav/systemAdministrationOverview/');
     cy.url().should('include', '/systemAdministrationOverview');
     cy.get('button:contains("Reports")').eq(1).click();
-    cy.get('button').contains(/^ Placements$/).closest('button').click();
+    cy.get('button')
+      .contains(/^ Placements$/)
+      .closest('button')
+      .click();
     cy.get('button').contains('Total Placements').closest('button').click();
     cy.url().should('include', 'reportOnCampusPlacements/main');
-  })
+  });
 
   it('Filter by year level', () => {
-    cy.get('input[formcontrolname=yearLevel]').click(); 
+    cy.get('input[formcontrolname=yearLevel]').click();
     cy.get('mat-option').contains('3rd years').click();
+    // Apply the filter
     cy.get('button#ci285081').click();
-    
-  })
-
-})
+    // Loading before filters are updated
+    cy.wait(6000);
+    // For each row in the table, verify that the year level column contains '3rd years'
+    cy.get('table tbody tr').each(($row) => {
+      cy.wrap($row)
+        .find('td')
+        .eq(2) // Assuming the year level is in the 3rd column (index 2)
+        .should('contain.text', '3');
+    });
+  });
+});
